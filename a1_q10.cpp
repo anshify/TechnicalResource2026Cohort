dad2d26fa7bdca1ae1cf1e0faca302c2bf644047
anshify <119318592+anshify@users.noreply.github.com>
2023-01-15 13:42:05 +0530

    Submitting Q10 of Assignment1

---
+++ a1_q10.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+
+    //write your code here
+    while(n % 2 == 0){
+        cout<<2<<" ";
+        n = n / 2;
+    }
+     for (int i = 3; i <= sqrt(n); i += 2 ){
+         while(n % i == 0){
+           cout<<i<<" ";
+           n = n/i;
+         }
+     }
+     
+     if (n > 2)
+        cout << n << " ";
+     
+     return 0;
+}
